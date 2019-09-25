e55f91eb7ecb14007e931082c21ae85f8092251c
Md Imran <md.imranh1987@gmail.com>
2019-09-25 17:10:51 +0600

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 
+Route::get('/', function () {
+return view('dashboard.index');
+})->name('home');
+
+// Route::get('/colors',function (){
+//     return view('dashboard.pages.colors');
+// });
+Route::get('/form',function (){
+return view('front-end.form.index');
+});
+
+Route::get('/form/view',function (){
+return view('front-end.form.view');
+});
+
+Route::get('/payments',function (){
+return view('front-end.form.payment');
+});
 
 Route::get('/accounts',function (){
-    return view('dashboard.account.index');
+return view('dashboard.account.index');
 });
 
 
@@ -12,5 +30,7 @@ Route::get('/clear',function(){
     Artisan::call('config:cache');
     Artisan::call('cache:clear');
 });
+
+<!--checking pull-->
 
 
